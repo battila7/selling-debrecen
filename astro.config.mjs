57b38f1e03c57ae1fdc20bcad2db5f46f4b431b8
Attila Bagossy <bagossyattila@outlook.com>
2025-11-10 13:27:09 +0100

    chore: added them deps and configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+import tailwindcss from "@tailwindcss/vite";
+
+import sitemap from "@astrojs/sitemap";
+
+import playformCompress from "@playform/compress";
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [sitemap(), playformCompress()],
+  compressHTML: true,
+  output: "static",
+  site: "https://selling-debrecen.hu",
+});
