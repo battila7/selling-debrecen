b14609c90895561d940a7f3c99c9a74dc890347e
Attila Bagossy <bagossyattila@outlook.com>
2025-11-11 13:18:18 +0100

    chore: checkpoint

--- src/components/page-specific/index-page/index.ts
+++ src/components/page-specific/index-page/index.ts
@@ -1,4 +1,4 @@
 export { default as Hero } from "./Hero.astro";
 export { default as FeaturedListings } from "./FeaturedListings.astro";
 export { default as Intro } from "./Intro.astro";
-export { default as Services } from "./Services.astro";
+export { default as Services } from "./services/Services.astro";
