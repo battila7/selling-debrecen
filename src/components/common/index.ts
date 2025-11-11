d0c664d8751049249276ff838220e4f9bf3deb5b
Attila Bagossy <bagossyattila@outlook.com>
2025-11-11 15:27:36 +0100

    refactor: extracted the mobile overlay

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,5 @@
 export { default as Head } from "./Head.astro";
-export { default as Header } from "./Header.astro";
+export { default as Header } from "./header/Header.astro";
 export { default as Contact } from "./contact/Contact.astro";
 export { default as Footer } from "./Footer.astro";
 export { default as HeadingCentered } from "./HeadingCentered.astro";
