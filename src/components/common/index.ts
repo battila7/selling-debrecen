599d28b55570f933dc30c3eefc74100572343b3c
Attila Bagossy <bagossyattila@outlook.com>
2025-11-11 14:35:18 +0100

    feat: proper contact section

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,4 +1,6 @@
 export { default as Head } from "./Head.astro";
 export { default as Header } from "./Header.astro";
-export { default as Contact } from "./Contact.astro";
+export { default as Contact } from "./contact/Contact.astro";
 export { default as Footer } from "./Footer.astro";
+export { default as HeadingCentered } from "./HeadingCentered.astro";
+export { default as HeadingLeftAligned } from "./HeadingLeftAligned.astro";
