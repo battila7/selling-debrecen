9d16d73aa839779f28eb6ef20942c3d5ff98b692
Attila Bagossy <bagossyattila@outlook.com>
2025-11-12 12:00:58 +0100

    refactor: the index header situation is now okay

--- src/components/page-specific/index-page/index.ts
+++ src/components/page-specific/index-page/index.ts
@@ -2,3 +2,4 @@ export { default as Hero } from "./Hero.astro";
 export { default as FeaturedListings } from "./FeaturedListings.astro";
 export { default as Intro } from "./Intro.astro";
 export { default as Services } from "./services/Services.astro";
+export { default as MobileHeader } from "./MobileHeader.astro";
